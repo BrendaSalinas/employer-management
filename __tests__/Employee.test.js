109a963418a461c62ebb90b2dcbda0eeeff3bff5
Brenda Salinas <brenda.salinas2128@gmail.com>
2022-01-18 06:22:13 -0600

    Created the employee.test, test failed

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,24 @@
+const Employee = require('../lib/Employee');
+const employee = new Employee('Brenda', '546', 'brenda.salinas2128@gmail.com');
+
+test('we are testing constructor values', () => {
+    expect(employee.name).toBe('Brenda');
+    expect(employee.id).toBe('546');
+    expect(employee.email).toBe('brenda.salinas2128@gmail.com')
+});
+
+test('we are testing the name function getName()', () => {
+    expect(employee.getName()).toBe('Brenda');
+});
+
+test('we are testing the id function getId()', () => {
+    expect(employee.getId().toBe('546'));
+});
+
+test('we are testing the email function getEmail()', () => {
+    expect(employee.getEmail().toBe('brenda.salinas2128@gmail.com'));
+});
+
+test('we are testing the role function getRole()', () => {
+    expect(employee.getRole().toBe('Employee'));
+});
