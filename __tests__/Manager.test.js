ece1c63e27ec787de1e1526532532589ae741822
Brenda Salinas <brenda.salinas2128@gmail.com>
2022-01-22 11:21:41 -0600

    adding the manager test to fail

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,29 @@
+const Manager = require('../lib/Manager');
+const manager = new Manager('Triston', '536', 'triston.beadle@sovosbrands.com','3');
+
+test('we are testing constructor values', () => {
+    expect(manager.name).toBe('Triston');
+    expect(manager.id).toBe('536');
+    expect(manager.email).toBe('triston.beadle@sovosbrands.com');
+    expect(manager.officeNumber).toBe('3');
+});
+
+test('we are testing the name function getName()', () => {
+    expect(manager.getName()).toBe('Triston');
+});
+
+test('we are testing the id function getId()', () => {
+    expect(manager.getId()).toBe('536');
+});
+
+test('we are testing the email function getEmail()', () => {
+    expect(manager.getEmail()).toBe('triston.beadle@sovosbrands.com');
+});
+
+test('we are testing the role function getRole()', () => {
+    expect(manager.getRole()).toBe('Manager');
+});
+
+test('we are testing the function officeNumber()', () => {
+    expect(manager.officeNumber()).toBe('3');
+});
