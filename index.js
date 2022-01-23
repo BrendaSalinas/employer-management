1ced4fd721177f3de8bd4d93108a5beff180f4cc
Brenda Salinas <brenda.salinas2128@gmail.com>
2022-01-23 09:09:03 -0600

    generate objects for each position

---
+++ index.js
@@ -0,0 +1,41 @@
+const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const { prompt } = require('inquirer');
+const path = require('path');
+const fs = require('fs');
+
+const roles = {
+    Employee: [],
+    Manager: [],
+    Engineer: [],
+    Intern: []
+};
+
+prompt(
+    {
+        type:'list',
+        name:'role',
+        message:'Which role would you like to fill?',
+        choices:Object.keys(roles)
+    }
+).then(({role})=>{
+    let items = ['name','id','email'];
+    // items.push(role=='Manager'?'officeNumber':role=='Engineer'?'github':role=='Intern'?'school');
+    if (role == 'Manager' ) {
+        items.push('officeNumber')
+    } else if (role =='Engineer') {
+        items.push('github')
+    }else if (role=='Intern') {
+        items.push('school')
+    };
+    let questions = [];
+
+    items.forEach(item => {
+        questions.push({type:'input',name:item,message:`What is your ${item}?`})
+    });
+
+    prompt(questions).then(console.log);
+});
+
